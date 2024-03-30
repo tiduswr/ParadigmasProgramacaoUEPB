a7e6fcf1ca4f1ccf21f9628040dc97cf590619e4
Harllem Nascimento <harllemnascimento@gmail.com>
2024-03-30 01:05:37 -0300

    cria projeto para aula de Prog. Orientada a Eventos

---
+++ orientacao-a-eventos/orientacao-eventos/src/main/java/com/tiduswr/Login.java
@@ -0,0 +1,80 @@
+package com.tiduswr;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Login extends JDialog {
+    private JTextField txtName;
+    private JLabel lblName;
+    private JPasswordField txtSenha;
+    private JLabel lblSenha;
+    private JPanel bottom;
+    private JPanel center;
+    private JPanel background;
+    private JButton okButton;
+    private JButton sairButton;
+
+    private final int ITENS_HEIGHT = 30;
+    private final Dimension BUTTON_SIZE = new Dimension(100, ITENS_HEIGHT);
+    private final Dimension SCREEN_SIZE = new Dimension(350, 400);
+
+    public Login(JFrame parent) throws Exception {
+        super(parent);
+        init(parent);
+    }
+
+    private void init(JFrame parent) throws Exception {
+        setTitle("Login");
+        setContentPane(background);
+        setSize(SCREEN_SIZE);
+        txtSenha.setMinimumSize(BUTTON_SIZE);
+        txtName.setMinimumSize(BUTTON_SIZE);
+        okButton.setMinimumSize(BUTTON_SIZE);
+        sairButton.setMinimumSize(BUTTON_SIZE);
+        setMinimumSize(SCREEN_SIZE);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+
+        var keyListener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {}
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                var c = keyEvent.getKeyChar();
+
+                if(Character.isLetterOrDigit(c))
+                    System.out.println("Você digitou a tecla '" + c + "'!");
+
+                if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+                    fazerLogin();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {}
+        };
+
+        txtName.addKeyListener(keyListener);
+        txtSenha.addKeyListener(keyListener);
+        okButton.addActionListener(event -> fazerLogin());
+        sairButton.addActionListener(event -> dispose());
+    }
+
+    private void fazerLogin(){
+        String usuario = txtName.getText();
+        String senha = new String(txtSenha.getPassword());
+
+        if (usuario.equals("admin") && senha.equals("123")) {
+            JOptionPane.showMessageDialog(Login.this, "Login bem-sucedido!");
+        } else {
+            JOptionPane.showMessageDialog(Login.this, "Credenciais inválidas. Tente novamente.");
+        }
+
+        dispose();
+    }
+
+}
---
+++ orientacao-a-eventos/orientacao-eventos/src/main/java/com/tiduswr/Main.java
@@ -0,0 +1,7 @@
+package com.tiduswr;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new Login(null);
+    }
+}
